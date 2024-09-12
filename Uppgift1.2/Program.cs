67fc19aa2353616928c61ca2019ae8740d03030f
maol07001 <maol07001@E-PW05CGR4>
2024-09-12 14:24:16 +0200

    Add project files.

---
+++ Uppgift1.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Uppgift_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White; Console.WriteLine("Memmory slot \"2\" has been corupded");
+            Console.WriteLine("Memmory number 8275818 eror");
+            Console.ReadLine();
+        }
+    }
+}
